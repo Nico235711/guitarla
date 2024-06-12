80a4df9b8514e808f21fa2e9fc595bd565b8d3bb
Nicolás Lopez Vidueiros <139637937+Nico235711@users.noreply.github.com>
2024-06-12 15:51:17 -0300

    Mostrando los Contenidos del carrito

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ const App = () => {
 
   return (
     <>
-      <Header />
+      <Header cart={cart} />
 
       <main className="container-xl mt-5">
         <h2 className="text-center">Nuestra Colección</h2>
