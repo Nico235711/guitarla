f548719835d4c10e853970642ecbe1cc7e0176d7
Nico235711 <139637937+Nico235711@users.noreply.github.com>
2025-03-18 15:05:45 -0300

    preparando el entorno

--- Dockerfile
+++
@@ -1,5 +0,0 @@
-FROM node:22.14.0-bullseye
-WORKDIR /app
-COPY . .
-RUN npm install
-CMD [ "npm", "run", "dev", "--", "--host" ]
--- src/utils/formatCurrency.js
+++
@@ -1,6 +0,0 @@
-export const formatCurrency = (amount) => {
-  return new Intl.NumberFormat("es-US", {
-    style: "currency",
-    currency: "USD"
-  }).format(amount)
-}
